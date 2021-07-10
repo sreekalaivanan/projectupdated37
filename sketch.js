ce456299863d92e9b7b8bab7cee112f9e7e68ce9
sreekalaivanan <73590949+sreekalaivanan@users.noreply.github.com>
2021-07-10 19:55:50 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var gameState,contestantCount,database,quiz,question,contestant;
+gameState = 0;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
